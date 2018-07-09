66290c7e0910466abb49f43d94698cb965315c8d
ida <poohmaki0506@gmail.com>
2018-07-09 09:35:29 +0900

    api叩くところ完成

--- src/toKalliopeia/create_post.py
+++ src/toKalliopeia/create_post.py
@@ -1,16 +1,22 @@
 import simplejson as json
+from .fetch_api import create_post
 
 
-def _post_post(f):
+def _post_post(f, token):
     ff = f.open("r")
     jsonData = json.load(ff)
-
+    data = {}
+    data = {"body": jsonData["body"],
+            "in_reply_to_id": jsonData["in_reply_to_id"]}
+    create_post(token=token, data=data)
 
     f.unlink()
 
-def create_post_main(fn):
+
+def create_post_main(fn, token):
     # ファイルないの投稿をすべて投稿し、ファイルを空にする
     print("create_post")
     f_lists = list(fn.glob("*.json"))
     for f in f_lists:
-        _post_post(f)
+        _post_post(f, token)
+        f.unlink()
