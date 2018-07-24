864d35e2e9101a957c32c33db36ec473a6782656
ida <poohmaki0506@gmail.com>
2018-07-24 13:25:54 +0900

    bondのバグ取り

--- src/question_generator/q_no_premise.py
+++ src/question_generator/q_no_premise.py
@@ -40,7 +40,7 @@ def _bond_last_word(p_phs, si):
     if b_last_word.pos_detail == "読点":
         return True
 
-    t_phs = p_phs[si + 1]
+    t_phs = p_phs[si]
     if len(t_phs) < 3:
         return True
 
@@ -57,11 +57,12 @@ def _check_bond(pi, post, target_si, f_mrph):
     before_si = target_si
     while before_si > 0:
         if _bond_last_word(p_phs, before_si):
-            bond_si_list.append(before_si)
+            bond_si_list.append(before_si-1)
         else:
             break
         before_si -= 1
-
+    if len(bond_si_list) == 0 and len(post.sentences[target_si].body) < 3 and target_si != 0:
+        bond_si_list.append(target_si-1)
     # 対象の文
     bond_si_list.append(target_si)
 
@@ -72,17 +73,18 @@ def _check_bond(pi, post, target_si, f_mrph):
     print("          after_si", after_si, s_num)
     while after_si < s_num:
         if _bond_last_word(p_phs, after_si):
-            bond_si_list.append(after_si)
+            bond_si_list.append(after_si-1)
         else:
             break
         after_si += 1
     print("bond_si_list", bond_si_list)
-    return sorted(bond_si_list)
+    return sorted(list(set(bond_si_list)))
 
 
 def no_premise_q_generator(post, pi, s, si, fn_templates, fn_mrph):
     # 前提が多すぎる場合
     if len(s.has_premise) > 1:
+        print("      it has enough premise")
         return None
 
     si_list = _check_bond(pi, post, si, fn_mrph)
