710589efa6cce8aca78830359b42416808a20677
ida <poohmaki0506@gmail.com>
2018-09-27 19:17:38 +0900

    ファイルを移動

--- src/create_usrs.py
+++ src/create_usrs.py
@@ -6,7 +6,7 @@ import toKalliopeia
 
 def main():
     token = toKalliopeia.get_access_token("facilitator", "test")
-    f_user = Path("201810_users.json")
+    f_user = Path("user_list_201810.json")
 
     f = f_user.open("r")
     jsonData = json.load(f)
